22a69ee86bf31325f16f5477494a2d73086f222e
Jean-Christophe Saad-Dupuy <saad.dupuy@gmail.com>
2013-11-24 16:32:07 +0100

    pylint

--- src/clu/common/base/configurable.py
+++ src/clu/common/base/configurable.py
@@ -1,47 +1,51 @@
-#!/usr/env/bin python2
+""" Module containing baase classes for configurables items"""
 
 class AutoConfigurableException(Exception):
+  """
+  Exceptions raised by AutoConfigurable en Configurable classes
+  """
   pass
 
 class AutoConfigurable(object):
-  ''' Ths classs allow to valorize attributes with any given parameters'''
+  """ This classs allow to valorize attributes with any given parameters"""
 
   def __init__(self, *args, **kwargs):
-    '''If using *args, first argument must be a dictionnary'''
+    """If using *args, first argument must be a dictionnary"""
     if len(args) > 1:
       raise AutoConfigurableException("Only one argument in *args and must be a dict")
     if len(args)> 0 and not type(args[0]) == dict:
       raise AutoConfigurableException("**args must be a dict ")
 
-    myattr={}
+    myattr = {}
     if len(args)>0 :
-      myattr=args[0]
+      myattr = args[0]
     else:
-      myattr=kwargs
+      myattr = kwargs
 
     for k in myattr:
       val = myattr[k]
-      if type(val)==dict:
-        self.__dict__[k]=AutoConfigurable(val)
+      if type(val) == dict:
+        self.__dict__[k] = AutoConfigurable(val)
       else:
-        self.__dict__[k]=val
+        self.__dict__[k] = val
 
 
 class Configurable(object):
-  def __init__(self, config={}):
-    self.config=AutoConfigurable(config)
-  
-  def __neededs__(self, needed=()):
+  """ Class containing a 'config' attribute (AutoConfigurable)"""
+  def __init__(self, config):
+    self.config = AutoConfigurable(config)
+
+  def __neededs__(self, needed = ()):
     for k in needed:
       if not self.config.__dict__.has_key(k):
         raise AutoConfigurableException("missing %s"%(k))
 
-  def __defaults__(self, defaults={}):
+  def __defaults__(self, defaults):
     for k in defaults:
       if not self.config.__dict__.has_key(k):
-        val=defaults[k]
-        if type(val)==dict:
-          self.config.__dict__[k]=AutoConfigurable(val)
+        val = defaults[k]
+        if type(val) == dict:
+          self.config.__dict__[k] = AutoConfigurable(val)
         else:
-          self.config.__dict__[k]=val
+          self.config.__dict__[k] = val
 
